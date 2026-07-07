c9aeb71fe0905654deb8d0055d7a49c682fd0207
DJ Mahir Nation TV <78599090+DJMahirNationTV@users.noreply.github.com>
2026-07-07 10:22:46 +0200

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+import validator.validation;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+    public static void function(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+    }
+}
---
+++ src/main/java/validator/validation.java
@@ -0,0 +1,29 @@
+package validator;
+
+public class validation {
+    public static boolean hasMinLength(String password, int min) {
+        return false;
+    }
+
+    public static boolean containsDigit(String password) {
+        return false;
+    }
+
+    public static boolean containsUpperAndLower(String password) {
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) // kleine interne Liste
+    {
+        return false;
+    }
+
+    public static boolean containsSpecialChar(String password, String allowed) {
+        return false;
+    }
+
+    public static boolean isValid(String password) // nutzt die obenstehenden Checks
+    {
+        return false;
+    }
+}
